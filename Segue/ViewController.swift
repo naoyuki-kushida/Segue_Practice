1b37483467cbbfb5d2317fc0406016a68f45d34d
Naoyuki Kushida <naoyuki-kushida@nova-system.com>
2021-09-16 13:00:14 +0900

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Owner on 2021/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
